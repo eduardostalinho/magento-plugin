cd209955badfab80bc3e8b07e28cb77da058720b
Fábio Henrique <fabiohf7@gmail.com>
2016-04-27 16:47:28 -0300

    ignora pasta .idea

---
+++ app/code/community/OmniBuy/Prognoos/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @category    OmniBuy
+ * @package     OmniBuy_Prognoos
+ * @author      Fábio <fabio@constance.com.br>
+ */
+class OmniBuy_Prognoos_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 

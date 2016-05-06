d8e57fd97f256d28d822f59e947b57b21d622364
Fabio H <fabiohf7@gmail.com>
2016-05-06 08:13:21 -0300

    melhorias no codigo

--- app/code/community/OmniBuy/Prognoos/Helper/Data.php
+++ app/code/community/OmniBuy/Prognoos/Helper/Data.php
@@ -21,7 +21,53 @@ class OmniBuy_Prognoos_Helper_Data extends Mage_Core_Helper_Abstract
         }
 
         $allCategories = substr($allCategories, 0, -1);
+
         return $allCategories;
+    }
+
+    public function getTemplate($pageName, $showcaseType)
+    {
+        $status = null;
+
+        switch ($showcaseType) {
+            case OmniBuy_Prognoos_Model_ShowcaseTypes::WHO_SAW_ALSO_SAW:
+                $status = Mage::getStoreConfig('vitrines/' . $pageName . '/enable_who_saw_also_saw');
+
+                if ($status == true) {
+                    Mage::app()->getLayout()->createBlock('core/template')
+                        ->setTemplate('prognoos/showcase/who-saw-also-saw.phtml');
+                }
+
+                return $status;
+                break;
+            case OmniBuy_Prognoos_Model_ShowcaseTypes::RECOMMENDED:
+                $status = Mage::getStoreConfig('vitrines/' . $pageName . '/enable_recommended');
+
+                if ($status == true) {
+                    Mage::app()->getLayout()->createBlock('core/template')
+                        ->setTemplate('prognoos/showcase/recommended.phtml');
+                }
+
+                return $status;
+                break;
+            case OmniBuy_Prognoos_Model_ShowcaseTypes::THE_MOST_VISITED:
+                $status = Mage::getStoreConfig('vitrines/' . $pageName . '/enable_most_visited');
+
+                if ($status == true) {
+                    Mage::app()->getLayout()->createBlock('core/template')
+                        ->setTemplate('prognoos/showcase/top-visited.phtml');
+                }
+
+                return $status;
+                break;
+            default:
+                return 0;
+        }
+    }
+
+    public function getPrognoosUser()
+    {
+        return Mage::getStoreConfig('geral/geral/user_prognoos');
     }
 }
 	 
